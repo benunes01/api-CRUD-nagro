dd6254a371274c1da825f1dfd5a992851992569f
benunes01 <be_nunes_pinheiro@hotmail.com>
2020-01-21 22:28:59 -0300

    Configurei o sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'teste',
+    database: 'nagrodatabase',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    }
+};
