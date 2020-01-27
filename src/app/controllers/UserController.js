9d63809245391286859c4f9e9143a3deb24b143a
benunes01 <be_nunes_pinheiro@hotmail.com>
2020-01-27 01:07:37 -0300

    Uso do Yup para melhorar as validações e criação da listagem de usuarios

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,26 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
+    
+    async index(req, res) {
+        const users = await User.findAll({ attributes: ['id', 'name', 'email', 'cpf'] });
+
+        return res.json(users);
+    }
+
     async store(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string().email().required(),
+            email: Yup.string().required(),
+            cpf: Yup.string().required().min(11),
+            password: Yup.string().required().min(6)
+        });
+
+        if (!(await schema.isValid(req.body))){
+            return res.status(400).json({ error: 'Validation Fails.' });
+        }
+
         const userExists = await User.findOne({ where: { email: req.body.email } });
 
         if (userExists) {
@@ -19,6 +38,24 @@ class UserController {
     }
 
     async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            cpf: Yup.string().min(11),
+            oldPassword: Yup.string().min(6),
+            password: Yup.string().min(6).when('oldPassword', (oldPassword, field) => 
+                oldPassword ? field.required() : field
+            ),
+            confirmPassword: Yup.string().when('password', (password, field) => 
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            )
+        });
+
+        if (!(await schema.isValid(req.body))){
+            return res.status(400).json({ error: 'Validation Fails.' });
+        }
+
+
         const { email, oldPassword } = req.body;
 
         const user = await User.findByPk(req.userId);
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
+routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
